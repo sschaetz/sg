13efcd66bec1430fb47736afce5dd26d80642bab
Your Name <seb.schaetz@gmail.com>
2010-11-03 01:59:28 +0100

    Moved completely to jquery and to Qunit Added first testcases for Qunit Added trick to clientdata to not require globals

--- app/clientdata.js
+++ app/clientdata.js
@@ -2,25 +2,15 @@
  * This is the main data structure of the client appliaction
  */
 
-function inviteFriend(url, username, alias, localkey, localsecret)
-{
-  this.url = url;
-  this.username = username;
-  this.alias = alias;
-  this.localkey = localkey;
-  this.localsecret = localsecret;
-  this.status = 0;
-}
-
 function Clientdata_friend()
 {
-  this.url          = unset_value;
-  this.username     = unset_value;
-  this.alias        = unset_value;
-  this.localkey     = unset_value;
-  this.remotekey    = unset_value;
-  this.sharedsecret = unset_value;
-  this.localsecret  = unset_value;
+  this.url          = "";
+  this.username     = "";
+  this.alias        = "";
+  this.localkey     = "";
+  this.remotekey    = "";
+  this.sharedsecret = "";
+  this.localsecret  = "";
   /* status of friends: -1 = notset, 0 = sent invite, 1 = friends, 
       2 = sent invite but declined, 3 = received invite but declined 
       4 = ignoring */
@@ -32,8 +22,28 @@ function Clientdata()
   this.url          = unset_value;
   this.writekey     = unset_value;
   this.friends      = Array();
-  this.inviteFriend = inviteFriend;
 }
 
+sg = function () 
+{
+
+	// private member variable
+	var clientdata = new Clientdata();
+	
+	// a private method
+	var privatemethod = function () 
+	{
+	}
+	
+	return  {
+	  // public member variable
+		publicproperty: "asdf",
+		// public method
+		publicmethod: function () 
+		{
+		}
+	};
+
+}();
 
 
