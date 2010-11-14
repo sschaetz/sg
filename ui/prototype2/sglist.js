ac3ed76d9f4c7be1a5fc00062c6923d507cef489
Your Name <seb.schaetz@gmail.com>
2010-11-14 19:13:27 +0100

    Added add/remove methods to list ui widget Added click event handling to ui widget

--- ui/prototype2/sglist.js
+++ ui/prototype2/sglist.js
@@ -1,3 +1,12 @@
+/*
+$('#x02 ul').click(function (e) {
+	if ($(e.target).parent().parent().is('li'))
+		$(e.target).parent().parent().toggleClass('highlight');
+	return false;
+});
+http://lab.distilldesign.com/event-delegation/#x02
+*/
+
 (function( $, undefined ) {
 
 $.widget("ui.sglist",
@@ -6,18 +15,66 @@ $.widget("ui.sglist",
   {
     listname: 'sglist'
 	},
+
 	_create: function() 
   {
-    // create a list
-    this.element.append('<ul class="' + this.options.listname + '-list"><ul>');
+    // create the list with event handler
+    $("<ul/>", {
+      "class": this.options.listname + "-list",
+      click: this._elementClick
+    }).appendTo(this.element);
+    
+    this.element.data('numElements', 0);
 	},
+
+  // private methods
+  
+  _pushTop: function(element, id)
+  {
+    var content = (typeof element === 'function') ? element() : element;
+
+    $("<li/>", {
+      text: content,
+      "class": this.options.listname + '-elem, ' + 
+               this.options.listname + '-elem-' + id
+    }).prependTo('.'+this.options.listname+ '-list').data('id', id);
+
+    this.element.data('numElements', this.element.data('numElements') + 1);
+  },
+
+  _remove: function(id)
+  {
+    $('.'+this.options.listname + '-elem-' + id).detach();
+    this.element.data('numElements', this.element.data('numElements') - 1);
+  },
+
+  _elementClick: function(e)
+  {
+	  if($(e.target).is('li'))
+		{
+      console.log($(e.target).data('id'));
+    }
+  },
 
 	destroy: function() 
   {
     this.element.empty();
 		return this;
-	}
+	},
 
+  // public methods
+  
+  // create a new element and push it to the top
+  pushTop: function(element, id) 
+  {
+    this._pushTop(element, id);
+  },
+  
+  // remove an element by its id
+  remove: function(id)
+  {
+    this._remove(id);
+  },
 
 });
 
