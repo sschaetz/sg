92cd519ed047c721da5d62bf94417513663cba5b
Your Name <seb.schaetz@gmail.com>
2010-12-01 23:12:30 +0100

    Added stub harness to test environment

--- app/clientdata.js
+++ app/clientdata.js
@@ -1,27 +1,54 @@
-/**
- * This is the main data structure of the client appliaction
- */
+Namespace('starbase.core');
+Namespace('starbase.core.communication');
+Namespace('starbase.core.secrets');
 
-function Clientdata_friend()
+
+starbase.core.init = 
+function(url, password, loginSalt, masterKeySalt)
 {
-  this.url          = "";
-  this.username     = "";
-  this.alias        = "";
-  this.localkey     = "";
-  this.remotekey    = "";
-  this.sharedsecret = "";
-  this.localsecret  = "";
-  /* status of friends: -1 = notset, 0 = sent invite, 1 = friends, 
-      2 = sent invite but declined, 3 = received invite but declined 
-      4 = ignoring */
-  this.status       = -1;
+  starbase.core.url = url;
+  starbase.core.secret.password = password;
+  starbase.core.secret.masterKeySalt = masterKeySalt;
+  starbase.core.secret.loginKey = starbase.hash.sha256(password, loginSalt);
 }
 
-function Clientdata()
+starbase.core.communication.loadData =
+function()
 {
-  this.url          = unset_value;
-  this.writekey     = unset_value;
-  this.friends      = Array();
+  $.ajax(
+  {
+    url: starbase.core.url,
+    data: 
+    {
+      loginkey: starbase.core.secret.loginKey
+    },
+    success: function(answer)
+    {
+      starbase.core.data = starbase.crypto.aes.decrypt(
+        starbase.core.secret.password, 
+        starbase.core.secret.masterKeySalt, answer.data);
+      console.log('data loaded!');
+    }
+  });
+}
+
+starbase.core.communication.saveData =
+function()
+{
+  $.ajax(
+  {
+    url: starbase.core.url, type: "POST",
+    data: 
+    {
+      data: starbase.crypto.aes.encrypt(starbase.core.secret.password, 
+        starbase.core.secret.masterKeySalt, starbase.core.data),
+      loginkey: starbase.core.secret.loginKey
+    },
+    success: function(answer)
+    {
+      console.log('data saved!');
+    }
+  });
 }
 
 sg = function () 
@@ -46,4 +73,27 @@ sg = function ()
 
 }();
 
+/**
+ * This is the main data structure of the client appliaction
+ */
+function Clientdata_friend()
+{
+  this.url          = "";
+  this.username     = "";
+  this.alias        = "";
+  this.localkey     = "";
+  this.remotekey    = "";
+  this.sharedsecret = "";
+  this.localsecret  = "";
+  /* status of friends: -1 = notset, 0 = sent invite, 1 = friends, 
+      2 = sent invite but declined, 3 = received invite but declined 
+      4 = ignoring */
+  this.status       = -1;
+}
+function Clientdata()
+{
+  this.url          = unset_value;
+  this.writekey     = unset_value;
+  this.friends      = Array();
+}
 
--- php/reset.php
+++ php/reset.php
@@ -1,8 +1,15 @@
 <?php
 
   include('app/inc/configuration.php');
+
+  $username = '';
+  if(isset($_GET['username']))
+  {
+    $username = $_GET['username'];
+  }  
+
   $conf = new Configuration();
-  exec("rm -rf " . $conf->full_install_directory);
+  exec("rm -rf " . $conf->full_install_directory . $username);
   header('Cache-Control: no-cache, must-revalidate');
   header('Content-type: application/json');
   echo json_encode
